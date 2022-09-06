7d161857ff1cec4432caad4e947b6f281d235beb
Brian Hu <bhurobin221@gmail.com>
2022-09-06 13:20:42 -0500

    modified install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ rm -rf .oh-my-zsh
 export RUNZSH="no"
 # install oh-my-zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+rm .zshrc*
 # install zsh plugins
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-rm .zshrc*
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 pushd $DOTFILES
