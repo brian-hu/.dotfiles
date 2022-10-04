22d910d9bed517ea095e4493760f6b0d73aa0bad
Brian Hu <bhurobin221@gmail.com>
2022-10-04 18:59:01 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env zsh
 
 pushd $HOME
-rm -rf .oh-my-zsh
-export RUNZSH="no"
-# install zinit
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
 pushd $DOTFILES
 for folder in * 
 do
